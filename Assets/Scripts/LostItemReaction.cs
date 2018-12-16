198b105e1f6518b89c5863fa1191f3d0f6a21102
Gerda Šimoliūnaitė <sml.gerda@gmail.com>
2018-12-16 20:52:53 +0200

    Added wall and inventory

---
+++ Assets/Scripts/LostItemReaction.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LostItemReaction : DelayedReaction {
+
+    public Item item;
+
+    private Inventory inventory;
+
+	protected override void SpecificInit()
+    {
+        inventory = FindObjectOfType<Inventory>();
+    }
+
+    protected override void ImmediateReaction()
+    {
+        inventory.RemoveItem(item);
+    }
+}
---
+++ Assets/Scripts/Raycast.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Raycast : MonoBehaviour {
+
+    private GameObject raycastedObj;
+
+    [SerializeField] private int rayLengt = 10;
+    [SerializeField] private LayerMask layerMaskInteract;
+    [SerializeField] private Image uiCrosshair;
+
+    void Update()
+    {
+        RaycastHit hit;
+        Vector3 fwd = transform.TransformDirection(Vector3.forward);
+
+        if(Physics.Raycast(transform.position, fwd, out hit, rayLengt, layerMaskInteract.value))
+        {
+            if (hit.collider.CompareTag("Object"))
+            {
+                raycastedObj = hit.collider.gameObject;
+                crosshairActive();
+
+                if (Input.GetKeyDown("e"))
+                {
+                    Debug.Log("I HAVE INTERACTED WITH AN OBJECT");
+                    raycastedObj.SetActive(false);
+                }
+            }
+        }
+        else
+        {
+            CrossHairNormal();
+        }
+    }
+
+    void crosshairActive()
+    {
+        uiCrosshair.color = Color.red;
+    }
+
+    void CrossHairNormal()
+    {
+        uiCrosshair.color = Color.white;
+    }
+}
