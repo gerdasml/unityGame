75fdb6350d97f973bd68cef166f56f18eebc432d
Gerda Šimoliūnaitė <sml.gerda@gmail.com>
2018-12-19 13:44:38 +0200

    Added interactable object

--- Assets/Scripts/Raycast.cs
+++ Assets/Scripts/Raycast.cs
@@ -10,6 +10,7 @@ public class Raycast : MonoBehaviour {
     [SerializeField] private int rayLengt = 10;
     [SerializeField] private LayerMask layerMaskInteract;
     [SerializeField] private Image uiCrosshair;
+    [SerializeField] private Inventory inventory;
 
     void Update()
     {
@@ -17,32 +18,46 @@ public class Raycast : MonoBehaviour {
         Vector3 fwd = transform.TransformDirection(Vector3.forward);
 
         if(Physics.Raycast(transform.position, fwd, out hit, rayLengt, layerMaskInteract.value))
+        //if (Physics.SphereCast(transform.position, 50, fwd, out hit, 0, layerMaskInteract.value))
         {
-            if (hit.collider.CompareTag("Object"))
+            if (hit.collider.CompareTag("InteractableObject")) 
             {
                 raycastedObj = hit.collider.gameObject;
-                crosshairActive();
+                crosshairActive(); // sita funkcija kvieciama, kai paziurim i objekta, turinti InteractableObject tag'a
 
                 if (Input.GetKeyDown("e"))
                 {
                     Debug.Log("I HAVE INTERACTED WITH AN OBJECT");
                     raycastedObj.SetActive(false);
+                    var shit = raycastedObj.GetComponent<InteractableObject>();
+                    var item = new Item();
+                    item.sprite = shit.sprite;
+                    inventory.AddItem(item);
+                    //Destroy(raycastedObj);
                 }
             }
         }
         else
         {
-            CrossHairNormal();
+            CrossHairNormal(); // sita funkcija kvieciama kai neziurim
         }
     }
 
     void crosshairActive()
     {
         uiCrosshair.color = Color.red;
+        if(raycastedObj != null)
+        {
+            raycastedObj.GetComponent<Renderer>().material.color = Color.cyan; // kai ziurim, pakeiciu objekto spalva i raudona
+        }
     }
 
     void CrossHairNormal()
     {
         uiCrosshair.color = Color.white;
+        if (raycastedObj != null)
+        {
+            raycastedObj.GetComponent<Renderer>().material.color = Color.white; // kitu atveju pakeiciu i balta (pasirodo tai tas pats kaip neturet spalvos kazkodel)
+        }
     }
 }
