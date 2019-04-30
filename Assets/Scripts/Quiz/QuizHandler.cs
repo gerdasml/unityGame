09cf401b788bce28bae7f29e4224baad71763465
Gerda Šimoliūnaitė <sml.gerda@gmail.com>
2019-04-30 15:57:59 +0300

    Added configurable fields to quiz puzzle

--- Assets/Scripts/Quiz/QuizHandler.cs
+++ Assets/Scripts/Quiz/QuizHandler.cs
@@ -4,37 +4,57 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class QuizHandler : MonoBehaviour {
-    private string Answer = "02-21";
+    
     public Text questionText;
     public Text pointsText;
     public List<Button> choiceButtons;
-    public int requiredAnswers;
+    public ScreenManager screenManager;
+    public GameObject tv;
+    public GameObject lockedObject;
 
-	// Use this for initialization
-	void Start () {
-        questionText.text = "Kada Vytoto gimtadienis?";
+    private int requiredAnswers;
+    private List<QuizQuestion> questions;
+    private int index = 0;
+    private string Answer;
+
+    // Use this for initialization
+    void Start () {
         pointsText.text = "0";
-        choiceButtons[0].GetComponentInChildren<Text>().text = "02-12";
-        choiceButtons[1].GetComponentInChildren<Text>().text = "04-17";
-        choiceButtons[2].GetComponentInChildren<Text>().text = "04-04";
-        choiceButtons[3].GetComponentInChildren<Text>().text = "02-21";
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+        questions = Config.Instance.Puzzle.Quiz.Questions;
+        questions.Shuffle();
+        FillButtonValues();
+        requiredAnswers = Config.Instance.Puzzle.Quiz.NumberOfAnswersNeeded;
+    }
 
+    private void FillButtonValues()
+    {
+        questionText.text = Config.Instance.Puzzle.Quiz.Questions[index].Text;
+        for(int i = 0; i < 4; i++)
+        {
+            var choice = Config.Instance.Puzzle.Quiz.Questions[index].Choices[i];
+            choiceButtons[i].GetComponentInChildren<Text>().text = choice.Text;
+            if (choice.IsCorrect) Answer = choice.Text;
+        }
+        index++;
+    }
+    
     public void HandleClick(Button button)
     {
         if(button.GetComponentInChildren<Text>().text == Answer)
         {
             pointsText.text = (int.Parse(pointsText.text) + 1).ToString();
-            Debug.Log("Guf");
-        } else
+            if (requiredAnswers == index)
+            {
+                screenManager.CloseScreen();
+                Destroy(tv.GetComponent<InteractableObject>());
+            }
+            FillButtonValues();
+        }
+        else
         {
             pointsText.text = "0";
-            Debug.Log("WA on test 1");
+            index = 0;
+            questions.Shuffle();
         }
     }
 }
