ab7b46eec4a5b11c3610beb70efbbf585929995c
Gerda Šimoliūnaitė <sml.gerda@gmail.com>
2019-05-03 13:59:42 +0300

    Added lightbulbs in quiz panel

--- Assets/Scripts/Handlers/QuizHandler.cs
+++ Assets/Scripts/Handlers/QuizHandler.cs
@@ -6,24 +6,34 @@ using UnityEngine.UI;
 public class QuizHandler : MonoBehaviour {
     
     public Text questionText;
-    public Text pointsText;
     public List<Button> choiceButtons;
     public ScreenManager screenManager;
     public GameObject tv;
     public GameObject lockedObject;
+    public GameObject lightbulbsPanel;
+    public Image lightbulbTemplate;
 
     private int requiredAnswers;
     private List<QuizQuestion> questions;
     private int index = 0;
     private string Answer;
+    private List<Image> lightbulbs = new List<Image>();
 
     // Use this for initialization
     void Start () {
-        pointsText.text = "0";
         questions = Config.Instance.Puzzle.Quiz.Questions;
+        requiredAnswers = Config.Instance.Puzzle.Quiz.NumberOfAnswersNeeded;
+
         questions.Shuffle();
         FillButtonValues();
-        requiredAnswers = Config.Instance.Puzzle.Quiz.NumberOfAnswersNeeded;
+
+        for(int i = 0; i < requiredAnswers; i++)
+        {
+            var lightbulb = Object.Instantiate(lightbulbTemplate);
+            lightbulb.gameObject.SetActive(true);
+            lightbulbs.Add(lightbulb);
+            lightbulb.transform.parent = lightbulbsPanel.transform;
+        }
     }
 
     private void FillButtonValues()
@@ -35,26 +45,30 @@ public class QuizHandler : MonoBehaviour {
             choiceButtons[i].GetComponentInChildren<Text>().text = choice.Text;
             if (choice.IsCorrect) Answer = choice.Text;
         }
-        index++;
     }
     
     public void HandleClick(Button button)
     {
         if(button.GetComponentInChildren<Text>().text == Answer)
         {
-            pointsText.text = (int.Parse(pointsText.text) + 1).ToString();
+            lightbulbs[index].color = Color.green;
+            index++;
             if (requiredAnswers == index)
             {
                 screenManager.CloseScreen();
                 Destroy(tv.GetComponent<InteractableObject>());
             }
+            
             FillButtonValues();
         }
         else
         {
-            pointsText.text = "0";
             index = 0;
             questions.Shuffle();
+            for(int i = 0; i < requiredAnswers; i++)
+            {
+                lightbulbs[i].color = Color.white;
+            }
         }
     }
 }
